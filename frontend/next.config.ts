a74e061a7ff35c4efd36451a0a9ce15bb0beacfb
kamvalethu100 <kamvalethu100@gmail.com>
2026-06-26 16:05:46 +0200

    next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // If you have specific eslint options, put them here
-  },
   output: 'standalone',
 };
 
